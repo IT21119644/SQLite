53797c807afa9fbc94413fe0562f3770e962a11e
IT21119644 <it21119644@my.sliit.lk>
2022-08-29 21:26:38 +0530

    Initial commit android studio

---
+++ app/src/main/java/com/example/sqliteapp/DBHelper.java
@@ -0,0 +1,79 @@
+package com.example.sqliteapp;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DBHelper extends SQLiteOpenHelper {
+    public DBHelper(Context context) {
+        super(context, "UserData.db", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase DB) {
+        DB.execSQL("CREATE TABLE UserDetails(ID TEXT PRIMARY KEY, name TEXT)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase DB, int i, int i1) {
+        DB.execSQL("DROP TABLE if exists UerDetails");
+    }
+
+    public boolean insertUserData(String ID, String name){
+        SQLiteDatabase DB = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("ID", ID);
+        contentValues.put("name", name);
+
+        long result = DB.insert("UserDetails", null, contentValues);
+        if(result == -1)
+            return false;
+        else
+            return true;
+    }
+
+    public boolean updateUserData(String ID, String name){
+        SQLiteDatabase DB = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("ID", ID);
+        contentValues.put("name", name);
+
+        Cursor cursor = DB.rawQuery("SELECT * FROM UserDetails WHERE id = ?", new String[] {ID});
+        if(cursor.getCount() > 0){
+            long result = DB.update("UserDetails", contentValues, "ID=?", new String[] {ID});
+            if(result == -1)
+                return false;
+            else
+                return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public boolean deleteUserData(String ID){
+        SQLiteDatabase DB = this.getWritableDatabase();
+
+        Cursor cursor = DB.rawQuery("SELECT * FROM UserDetails WHERE ID = ?", new String[] {ID});
+        if(cursor.getCount() > 0){
+            long result = DB.delete("UserDetails","ID=?", new String[] {ID});
+            if(result == -1)
+                return false;
+            else
+                return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public Cursor getData(){
+        SQLiteDatabase DB = this.getWritableDatabase();
+        Cursor cursor = DB.rawQuery("SELECT * FROM UserDetails", null);
+        return cursor;
+    }
+}
---
+++ app/src/main/java/com/example/sqliteapp/MainActivity.java
@@ -0,0 +1,94 @@
+package com.example.sqliteapp;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    EditText ID, name;
+    Button insert, read, update, delete;
+    DBHelper DB;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        name = findViewById(R.id.name);
+        ID = findViewById(R.id.ID);
+
+        insert = findViewById(R.id.insert);
+        update = findViewById(R.id.update);
+        delete = findViewById(R.id.delete);
+        read = findViewById(R.id.read);
+
+        DB = new DBHelper(this);
+    }
+
+    public void insertToDB(View v){
+        String IDSrc = ID.getText().toString();
+        String nameSrc = name.getText().toString();
+
+        boolean checkInsertData = DB.insertUserData(IDSrc, nameSrc);
+        if(checkInsertData){
+            Toast.makeText(MainActivity.this, "Entry updated", Toast.LENGTH_LONG).show();
+            ID.setText(null);
+            name.setText(null);
+        }
+        else
+            Toast.makeText(MainActivity.this, "New entry not inserted", Toast.LENGTH_LONG).show();
+    }
+
+    public void getDataFromDB(View v){
+        Cursor res = DB.getData();
+        if(res.getCount() == 0){
+            Toast.makeText(MainActivity.this, "No entry exists", Toast.LENGTH_LONG).show();
+            return;
+        }
+        StringBuffer buffer = new StringBuffer();
+        while(res.moveToNext()){
+            buffer.append("ID: " + res.getString(0) + "\n");
+            buffer.append("Name: " + res.getString(1) + "\n\n");
+        }
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setCancelable(true);
+        builder.setTitle("User Details");
+        builder.setMessage(buffer.toString());
+        builder.show();
+    }
+
+    public void deleteDataFromDB(View v){
+        String IDSrc = ID.getText().toString();
+        boolean deleteData = DB.deleteUserData(IDSrc);
+
+        if(deleteData){
+            Toast.makeText(MainActivity.this, "Entry deleted", Toast.LENGTH_LONG).show();
+            ID.setText(null);
+            name.setText(null);
+        }
+
+        else
+            Toast.makeText(MainActivity.this, "Entry cannot be deleted", Toast.LENGTH_LONG).show();
+    }
+
+    public void updateDataInDB(View v){
+        String IDSrc = ID.getText().toString();
+        String nameSrc = name.getText().toString();
+
+        boolean checkUpdatedData = DB.updateUserData(IDSrc, nameSrc);
+        if(checkUpdatedData){
+            Toast.makeText(MainActivity.this, "Entry updated", Toast.LENGTH_LONG).show();
+            ID.setText(null);
+            name.setText(null);
+        }
+        else
+            Toast.makeText(MainActivity.this, "Entry cannot be updated", Toast.LENGTH_LONG).show();
+    }
+}
