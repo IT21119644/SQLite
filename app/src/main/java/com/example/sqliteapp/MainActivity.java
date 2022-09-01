55ab2ef665f51454b820f6468642f8c7297db30e
IT21119644 <it21119644@my.sliit.lk>
2022-09-01 16:58:42 +0530

    Deleted CRUD operations

--- app/src/main/java/com/example/sqliteapp/MainActivity.java
+++ app/src/main/java/com/example/sqliteapp/MainActivity.java
@@ -24,7 +24,6 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
     EditText ID, name;
     Button insert, read, update, delete;
-    DBHelper DB;
     Spinner spinner;
 
     @Override
@@ -32,28 +31,13 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        name = findViewById(R.id.name);
-//        ID = findViewById(R.id.ID);
-//
-//        insert = findViewById(R.id.insert);
-//        update = findViewById(R.id.update);
-//        delete = findViewById(R.id.delete);
-//        read = findViewById(R.id.read);
-//
-//        //dropdown menu
-//        spinner = findViewById(R.id.spinner);
-//        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.languages, android.R.layout.simple_spinner_item);
-//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-//        spinner.setOnItemSelectedListener(this);
-//
-//        DB = new DBHelper(this);
-
         MaterialToolbar toolbar = findViewById(R.id.topAppBar);
         DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.navigation_view);
         toolbar.setNavigationOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
+
                 drawerLayout.openDrawer(GravityCompat.START);
             }
         });
@@ -74,6 +58,8 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
                         Toast.makeText(MainActivity.this, "Budget is clicked", Toast.LENGTH_SHORT).show();break;
                     case R.id.nav_goals:
                         Toast.makeText(MainActivity.this, "Goal is clicked", Toast.LENGTH_SHORT).show();break;
+                    case R.id.nav_login:
+                        Toast.makeText(MainActivity.this, "Login is clicked", Toast.LENGTH_SHORT).show();break;
                     default:
                         return true;
 
@@ -81,68 +67,6 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
                 return true;
             }
         });
-    }
-
-    public void insertToDB(View v){
-        String IDSrc = ID.getText().toString();
-        String nameSrc = name.getText().toString();
-
-        boolean checkInsertData = DB.insertUserData(IDSrc, nameSrc);
-        if(checkInsertData){
-            Toast.makeText(MainActivity.this, "Entry updated", Toast.LENGTH_LONG).show();
-            ID.setText(null);
-            name.setText(null);
-        }
-        else
-            Toast.makeText(MainActivity.this, "New entry not inserted", Toast.LENGTH_LONG).show();
-    }
-
-    public void getDataFromDB(View v){
-        Cursor res = DB.getData();
-        if(res.getCount() == 0){
-            Toast.makeText(MainActivity.this, "No entry exists", Toast.LENGTH_LONG).show();
-            return;
-        }
-        StringBuffer buffer = new StringBuffer();
-        while(res.moveToNext()){
-            buffer.append("ID: " + res.getString(0) + "\n");
-            buffer.append("Name: " + res.getString(1) + "\n\n");
-        }
-
-        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-        builder.setCancelable(true);
-        builder.setTitle("User Details");
-
-        builder.setMessage(buffer.toString());
-        builder.show();
-    }
-
-    public void deleteDataFromDB(View v){
-        String IDSrc = ID.getText().toString();
-        boolean deleteData = DB.deleteUserData(IDSrc);
-
-        if(deleteData){
-            Toast.makeText(MainActivity.this, "Entry deleted", Toast.LENGTH_LONG).show();
-            ID.setText(null);
-            name.setText(null);
-        }
-
-        else
-            Toast.makeText(MainActivity.this, "Entry not deleted", Toast.LENGTH_LONG).show();
-    }
-
-    public void updateDataInDB(View v){
-        String IDSrc = ID.getText().toString();
-        String nameSrc = name.getText().toString();
-
-        boolean checkUpdatedData = DB.updateUserData(IDSrc, nameSrc);
-        if(checkUpdatedData){
-            Toast.makeText(MainActivity.this, "Entry updated", Toast.LENGTH_LONG).show();
-            ID.setText(null);
-            name.setText(null);
-        }
-        else
-            Toast.makeText(MainActivity.this, "Entry not updated", Toast.LENGTH_LONG).show();
     }
 
     // dropdown menu methods
